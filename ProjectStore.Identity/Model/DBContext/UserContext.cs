48275657484d23bb5e7088e13e43d40e4968dcf2
Nuno <Nuno@DESKTOP-P3187RU>
2023-12-10 02:20:06 +0000

    Adicionar arquivos de projeto.

---
+++ ProjectStore.Identity/Model/DBContext/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjectStore.Identity.Model.DBContext
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Userdb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ ProjectStore.Identity/Model/User.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectStore.Identity.Model
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
